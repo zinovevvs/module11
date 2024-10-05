87ec00d1e37fc7820600d14095786385c573689f
zinovevvs <ziryanov97@gmail.com>
2024-10-05 16:33:21 +0500

    Update module11_2.py

--- module11_2.py
+++ module11_2.py
@@ -1,8 +1,10 @@
 def introspection_info(obj):
     obj_type = type(obj).__name__
-    attributes = dir(obj)
-    methods = [attr for attr in attributes if callable(getattr(obj, attr))]
-    obj_module = getattr(obj, '__module__', 'N/A') # Проверяею, есть ли у объекта атрибут __module__
+    all_attributes = dir(obj)
+
+    methods = [attr for attr in all_attributes if callable(getattr(obj, attr))]
+    attributes = [attr for attr in all_attributes if not callable(getattr(obj, attr))]
+    obj_module = getattr(obj, '__module__', '__main__') # Проверяю, есть ли у объекта атрибут __module__
 
     result = {
         'type': obj_type,
@@ -15,5 +17,6 @@ def introspection_info(obj):
 
 number_info = introspection_info(42)
 print(number_info)
+
 
 
